a9fc8d0f1a6521e9da556d569da8f2a4231cd885
hudaif99 <hudaifpaladan99@gmail.com>
2021-12-29 23:45:29 +0530

    Added names and abilities.

--- lib/imageAdd.dart
+++ lib/imageAdd.dart
@@ -11,16 +11,16 @@ class Avengers extends StatefulWidget {
 class _AvengersState extends State<Avengers> {
   List<Images> image = [];
   add(){
-    image.add(Images("assets/images/Bruce.png"));
-    image.add(Images("assets/images/Cap.png"));
-    image.add(Images("assets/images/Clint.png"));
-    image.add(Images("assets/images/Nat.png"));
-    image.add(Images("assets/images/Tchalla.png"));
-    image.add(Images("assets/images/Thor.png"));
-    image.add(Images("assets/images/Tony.png"));
-    image.add(Images("assets/images/Vision.png"));
-    image.add(Images("assets/images/Wanda.png"));
-    image.add(Images("assets/images/Scott.png"));
+    image.add(Images("assets/images/Bruce.png","Bruce Banner", "Incredible superhuman strength, \ndurability, and healing factor.\nBecomes more powerful as anger increases. "));
+    image.add(Images("assets/images/Cap.png", "Steve Rogers", "Superhuman Agility.\nSuperhuman Stamina.\nSuperhuman Durability.\nSuperhuman Reflexes.\nSuperhuman Strength."));
+    image.add(Images("assets/images/Clint.png", "Clint Barton", "Master archer and marksman \nExpert tactician, \nacrobat and hand-to-hand combatant Using a variety of trick arrows "));
+    image.add(Images("assets/images/Nat.png", "Natasha Romanoff", "She is skilled in many forms of martial arts including aikido, judo, karate, savate, and boxing. \nIn addition, she is an amazing gymnast, \nand uses her acrobatics to gain an edge in combat."));
+    image.add(Images("assets/images/Tchalla.png", "T'Challa", "The power to draw upon the knowledge, strength and every experience of every previous Black Panther.\nSuperhuman strength, endurance, speed, agility, reflexes, stamina and senses.\nExpert hand-to-hand combatant and martial artist."));
+    image.add(Images("assets/images/Thor.png", "Thor Odinson", "Superhuman strength, \nspeed, \ndurability and longevity \nAbilities via Mjolnir, \nInter-dimensional teleportation \nElectricity manipulation \nFlight Weather manipulation "));
+    image.add(Images("assets/images/Tony.png", "Tony Stark", "Genius level intellect Proficient scientist and engineer Powered armor suit: \nSupersonic flight Energy repulsor and missile projection Regenerative life support"));
+    image.add(Images("assets/images/Vision.png", "Vision", "Superhuman intelligence, strength, speed \nSuperhuman senses \nDensity control  \nFlight Regeneration \nSolar energy projection \nHolographic disguise \nTechnopathy"));
+    image.add(Images("assets/images/Wanda.png", "Wanda maximoff", "Reality warping, \nTelepathy, \nLevitation, \nEnergy absorption, \nEnergy projection"));
+    image.add(Images("assets/images/Scott.png", "Scott Lang", "Magic😉, \nAbility to shrink to microscopic size, \nAbility to grow really bigger"));
   }
   @override
   void initState() {
@@ -35,14 +35,30 @@ class _AvengersState extends State<Avengers> {
         title: Center(child: Text("Avengers")),
       ),
       body: ListView.builder(itemCount: image.length, itemBuilder: (context, index){
-        return Container(
+        return Container(width: MediaQuery.of(context).size.width,
+          //height: MediaQuery.of(context).size.height,
           margin: EdgeInsets.all(10),
-          child: Column(mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Image.asset(image[index].images),
+              Text(image[index].name, style: TextStyle(fontSize: 20,fontWeight: FontWeight.normal),),
+              Column(
+                //mainAxisAlignment: MainAxisAlignment.end,
+                //crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Row(
+                    //mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    //crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(image[index].abilities)
+                    ],
+                  ),
+                ],
+              )
             ],
           ),
+
         );
       }),
     );
@@ -51,5 +67,7 @@ class _AvengersState extends State<Avengers> {
 
 class Images{
   String images;
-  Images(this.images);
+  String name;
+  String abilities;
+  Images(this.images, this.name, this.abilities);
 }
